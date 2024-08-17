c3967f454f0532e99d1c94913a490fb261d49dc4
logesh-5179 <logeshmb5179@gmail.com>
2024-08-17 12:19:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const url = "https://jsonplaceholder.typicode.com/photos";
+  const [data, setData] = useState([]);
+  
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  
+ }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FETCHING API</h1>
+     
+        {data.map((dataObj) => {
+          return (
+            <div >
+              <ul>
+             <li>ID:{dataObj.id} </li>
+              <li> Name:  {dataObj.title} </li>
+             <li> Url: {dataObj.url}</li>
+            <li>thumbnail :<a href="https://via.placeholder.com/150/771796">{dataObj.thumbnailUrl}</a></li>
+              </ul>
+            </div>
+          );
+        })}
+    
     </div>
   );
 }
